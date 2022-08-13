dbf68e34b81df608fc3ead49096877d4930187a8
Sachin79 <amanraw2@gmail.com>
2022-08-13 23:30:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from telethon import TelegramClient
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-skeleton_url = os.environ.get('SKELETON_URL')
 auth_users = os.environ.get('AUTH_USERS').split()
 auth_groups = os.environ.get('AUTH_GROUPS').split()
 for i in range(len(auth_users)):
