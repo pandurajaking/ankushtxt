4ab6aa04576dedfdf69bc7c769d08f9419ef859f
Sachin79 <amanraw2@gmail.com>
2022-08-22 13:24:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
 import os
 from telethon import TelegramClient
-
+API_ID = 14560088
+API_HASH = "74a2665339484da3eaaed5f4fe16da79"
+BOT_TOKEN = "5524381543:AAH-s7TDhvA_Ng2k9U5z9pvgiRPy5ChNve8"
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
 bot_token = os.environ.get('BOT_TOKEN')
-auth_users = os.environ.get('AUTH_USERS').split()
-auth_groups = os.environ.get('AUTH_GROUPS').split()
-for i in range(len(auth_users)):
-    auth_users[i] = int(auth_users[i])
-for i in range(len(auth_groups)):
-    auth_groups[i] = int(auth_groups[i])
-
 bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
