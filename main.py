5e834891db3e3dc1aa799d864ed8972626c615d3
Sachin79 <amanraw2@gmail.com>
2022-08-22 14:23:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ from telethon import TelegramClient
 #API_ID = 14560088
 #API_HASH = "74a2665339484da3eaaed5f4fe16da79"
 #BOT_TOKEN = "5524381543:AAH-s7TDhvA_Ng2k9U5z9pvgiRPy5ChNve8"
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+#api_id = os.environ.get('API_ID')
+#api_hash = os.environ.get('API_HASH')
+#bot_token = os.environ.get('BOT_TOKEN')
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, sleep_threshold=120)
 
