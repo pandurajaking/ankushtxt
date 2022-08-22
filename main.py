d5afd6177f533477e7682241463febb3b0e0cca1
Sachin79 <amanraw2@gmail.com>
2022-08-22 13:48:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from telethon import events, Button
-from config import bot
+#from config import bot
 from FastTelethonhelper import fast_upload
 import os
 import subprocess
 import helper
 from telethon.tl.types import DocumentAttributeVideo
 
+
+
+import os
+from telethon import TelegramClient
+API_ID = 14560088
+API_HASH = "74a2665339484da3eaaed5f4fe16da79"
+BOT_TOKEN = "5524381543:AAH-s7TDhvA_Ng2k9U5z9pvgiRPy5ChNve8"
+api_id = os.environ.get('API_ID')
+api_hash = os.environ.get('API_HASH')
+bot_token = os.environ.get('BOT_TOKEN')
+
+
+bot = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
 
 cancel = False
 
